633df8d952d18f4cd759fcc3337e44822a4d05d8
Jan Yudintsev <406403@niuitmo.ru>
2024-11-13 01:13:14 +0300

    Добавлены недостающие файлы
    Добавил изменённые во 2 лабораторной работе коды в ветку develop, чтобы работать дальше с ними.

--- calculate.py
+++ calculate.py
@@ -1,33 +1,40 @@
 import circle
 import square
+import rectangle
+import triangle
 
-
-figs = ['circle', 'square']
+figs = ['circle', 'square', 'rectangle', 'triangle']
 funcs = ['perimeter', 'area']
-sizes = {}
+sizes = {
+    "area-circle": 1,
+    "perimeter-circle": 1,
+    "area-square": 1,
+    "perimeter-square": 1,
+    "area-rectangle": 2,
+    "perimeter-rectangle": 2,
+    "area-triangle": 2,
+    "perimeter-triangle": 3
+}
 
 def calc(fig, func, size):
-	assert fig in figs
-	assert func in funcs
+    assert fig in figs
+    assert func in funcs
 
-	result = eval(f'{fig}.{func}(*{size})')
-	print(f'{func} of {fig} is {result}')
+    result = eval(f'{fig}.{func}(*{size})')
+    print(f'{func} of {fig} is {result}')
 
 if __name__ == "__main__":
-	func = ''
-	fig = ''
-	size = list()
-    
-	while fig not in figs:
-		fig = input(f"Enter figure name, avaliable are {figs}:\n")
-	
-	while func not in funcs:
-		func = input(f"Enter function name, avaliable are {funcs}:\n")
-	
-	while len(size) != sizes.get(f"{func}-{fig}", 1):
-		size = list(map(int, input("Input figure sizes separated by space, 1 for circle and square\n").split(' ')))
-	
-	calc(fig, func, size)
+    func = ''
+    fig = ''
+    size = list()
 
+    while fig not in figs:
+        fig = input(f"Enter figure name, available are {figs}:\n")
 
+    while func not in funcs:
+        func = input(f"Enter function name, available are {funcs}:\n")
 
+    while len(size) != sizes.get(f"{func}-{fig}", 1):
+        size = list(map(int, input(f"Input figure sizes separated by space, expected {sizes.get(f'{func}-{fig}', 1)} values:\n").split(' ')))
+
+    calc(fig, func, size)
