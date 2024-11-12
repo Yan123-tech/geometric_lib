d8ff37689f647e2dd15af28ead04e830422610de
Jan Yudintsev <406403@niuitmo.ru>
2024-11-13 02:59:31 +0300

    Добавил проверку отрицательных значений
    Добавил проверку отрицательных значений во все программы для подсчёта математических параметров фигур

--- triangle.py
+++ triangle.py
@@ -1,5 +1,9 @@
 def area(a, h):
+    if a < 0 or h < 0:
+        raise ValueError("Base and height cannot be negative")
     return a * h / 2
 
 def perimeter(a, b, c):
+    if a < 0 or b < 0 or c < 0:
+        raise ValueError("Sides cannot be negative")
     return a + b + c
